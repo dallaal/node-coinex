49ed8479ce301e3c48afaa9dfb4359207cec43f7
sepehr Pourghannad <45392657+sepehrMSP@users.noreply.github.com>
2021-08-17 16:54:35 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ts-npm-package-boilerplate
+
---
+++ tests/some.test.ts
@@ -0,0 +1,11 @@
+import { cat } from '../src';
+
+describe('plus', () => {
+  it('should equal 2 for 1 plus 1', () => {
+    const expectedResult = 2;
+    expect(1 + 1).toEqual(expectedResult);
+  });
+  it('cat should say meow', () => {
+    expect(cat).toHaveProperty('say', 'meow');
+  });
+});
